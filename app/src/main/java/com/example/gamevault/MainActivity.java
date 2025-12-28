070cc87e82f0ab162c7f8f52be7f3aed2ef8c73c
Salateek2 <michal.rabinovich2015@gmail.com>
2025-12-28 19:10:56 +0200

    i added a search option

--- app/src/main/java/com/example/gamevault/MainActivity.java
+++ app/src/main/java/com/example/gamevault/MainActivity.java
@@ -8,17 +8,57 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.gamevault.adapter.GameAdapter;
+import com.example.gamevault.model.GameResponse;
+import com.example.gamevault.network.RawgApi;
+import com.example.gamevault.network.RetrofitClient;
+
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String API_KEY = "415d86e2c1bb4892be23a624f1955b6e";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerGames);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        RawgApi api = RetrofitClient
+                .getInstance()
+                .create(RawgApi.class);
+
+        api.getGames(API_KEY, 20).enqueue(new Callback<GameResponse>() {
+            @Override
+            public void onResponse(Call<GameResponse> call, Response<GameResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    GameAdapter adapter =
+                            new GameAdapter(response.body().results);
+                    recyclerView.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GameResponse> call, Throwable t) {
+                Toast.makeText(MainActivity.this,
+                        "Failed to load data",
+                        Toast.LENGTH_LONG).show();
+            }
         });
     }
 }
